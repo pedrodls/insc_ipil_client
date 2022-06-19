208525a8094ad93bb7768adbb462dc81324c3848
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-06-19 12:39:01 +0100

    Navbar default structure done

---
+++ src/layout/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
+//import { FaBars } from 'react-icons/fa';
+import { Logo } from '../../envs/imgs';
+
+export default function Navbar({ _class = "navbar" }) {
+    return (
+        <>
+
+            <nav className={styles[_class]}>
+                <div className="container-fluid">
+                    {/**------------------------------Menu------------------------------------- */}
+                    <div className="navbar-header d-flex justify-content-between align-items-center">
+                        <Link to={'/'}>
+                            <div className="logo-container">
+                                <div className="logo">
+                                    <img className={styles.logo} src={Logo} alt="Logotipo" />
+                                </div>
+                            </div>
+                        </Link>
+                        <button id="menu-toggle" type="button" className={styles['navbar-toggle'] + " btn btn-danger"}>
+                            {/*<FaBars />*/}
+                        </button>
+                        {/**------------------------------Colapso------------------------------------- */}
+                        <div className={styles.collapse + " " + styles['navbar-collapse']}>
+                            <ul className="nav d-flex">
+                                <li>
+                                    <Link to={'/signup'} className="btn btn-primary">Inscrição</Link>
+                                </li>
+                                <li>
+                                    &nbsp;&nbsp;<Link to={'/login'} className="btn btn-danger">Entrar</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
