16f541e3182fdbdc426d62605b0823bca2ee4b8e
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-06-19 12:36:31 +0100

    first elements structure import/export

---
+++ src/envs/elements.ts
@@ -0,0 +1,12 @@
+//----------------------Pages Documents--------------------------
+import Index from "../pages/index/Index";
+import Login from '../pages/login/Login';
+import SignUp from '../pages/signup/SignUp';
+
+export {Index, Login, SignUp};
+//---------------------------------------------------------------
+//---------------------Layouts Documents-------------------------
+import Navbar from '../layout/navbar/Navbar';
+
+export {Navbar}
+//---------------------------------------------------------------
