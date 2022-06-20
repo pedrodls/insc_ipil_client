f708c5a1a3fcedbed85168cb209bba5db9d1c02a
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-06-20 20:05:31 +0100

    select default structure tsx e css

---
+++ src/layout/select/Select.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Select.module.css';
+
+export default function Select({ defaultSelect = 0, text = "", id = "", _options = [], handle = () => { }, _class = "" }: any) {
+    return (
+        <>
+            {text && <label htmlFor={id}>{text}</label>}
+            <select defaultValue={defaultSelect} id={id} className={`${styles.select} ${styles[_class]} form-select shadow-none`} onChange={handle}>
+                {
+                    _options.length && _options.map((option: any, index: number) => (
+                        <option key={index} value={option.id}>{option.name}</option>
+                    ))
+                }
+            </select>
+        </>
+    )
+}
