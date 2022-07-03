28083716e0d2e91bbaefd5001e4eaa641ea1d2d7
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-07-03 12:30:35 +0100

    define phone, email, address in variables

--- src/envs/variables.ts
+++ src/envs/variables.ts
@@ -1,7 +1,10 @@
 const __VARIABLES__ = {
     _institute_name_: "INSTITUTO POLITÉCNICO INDUSTRIAL DE LUANDA",
     _institute_short_name_: "IPIL",
-    _default_theme: "#dc3445"
+    _default_theme_: "#dc3445",
+    _institute_phone_: '+244 934 054 182',
+    _institute_email_: 'ipil.makarenko.co.ao',
+    _institute_address_: 'Largo Sweto'
 }
 
 export default __VARIABLES__;
