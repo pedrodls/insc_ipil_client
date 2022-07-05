885ceadd1b9e436fb0ccf57cfef473ebd535bbe1
elisiomualumene <elisiomualumene@gmail.com>
2022-07-05 16:18:57 +0100

    :zap: admin

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Index, Login, SignUp, Home, Information, AttachPayment, Contact } from './envs/elements';
+import { Index, Login, SignUp, Home, Information, AttachPayment, Contact, Dashboard } from './envs/elements';
 
 
 function AppRoutes() {
@@ -9,14 +9,18 @@ function AppRoutes() {
     <Router>
       <Routes>
         <Route path="/" element={<Index />}>
-          <Route path="" element={<Home />} />
-          <Route path="/informations" element={<Information />} />
-          <Route path="/attach_payment" element={<AttachPayment />} />
-          <Route path="/contacts" element={<Contact />} />
+        <Route path="" element={<Home />} />
+        <Route path="/informations" element={<Information />} />
+        <Route path="/attach_payment" element={<AttachPayment />} />
+        <Route path="/contacts" element={<Contact />} />
         </Route>
         <Route path="/login" element={<Login />} />
 
         <Route path="/signup" element={<SignUp />} />
+
+        {/* ADMIN DASHBOARD */}
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        
       </Routes>
     </Router>
   )
