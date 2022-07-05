ca2a4186870f299d5ba4fdc6e7edc43c368258a1
pedrodls <pedrojoaodev@gmail.com>
2022-07-05 11:21:13 +0100

    Co-authored-by: Bruno Mateus <Bruno-F-D-Mateus@users.noreply.github.com> export elements to initial page

--- src/envs/elements.ts
+++ src/envs/elements.ts
@@ -3,7 +3,12 @@ import Index from "../pages/index/Index";
 import Login from '../pages/login/Login';
 import SignUp from '../pages/signup/SignUp';
 
-export {Index, Login, SignUp};
+import Information from "../pages/information/Information";
+import AttachPayment from "../pages/attach-payment/AttachPayment";
+import Contact from "../pages/contact/Contact";
+import Home from "../pages/home/Home";
+
+export {Index, Login, SignUp, Information, AttachPayment, Contact, Home};
 //---------------------------------------------------------------
 //---------------------Layouts Documents-------------------------
 import Navbar from '../components/navbar/Navbar';
@@ -13,5 +18,7 @@ import Input from '../components/input/Input';
 import Button from '../components/button/Button';
 import Select from '../components/select/Select';
 
+
 export {Navbar, Form, Input, Button, Select, Footer}
 //---------------------------------------------------------------
+
--- src/envs/variables.ts
+++ src/envs/variables.ts
@@ -1,10 +1,11 @@
 const __VARIABLES__ = {
     _institute_name_: "INSTITUTO POLITÉCNICO INDUSTRIAL DE LUANDA",
     _institute_short_name_: "IPIL",
-    _default_theme_: "#dc3445",
+    _orange_default_btn_: "orange",
     _institute_phone_: '+244 934 054 182',
     _institute_email_: 'ipil.makarenko.co.ao',
     _institute_address_: 'Largo Sweto'
 }
 
 export default __VARIABLES__;
+
