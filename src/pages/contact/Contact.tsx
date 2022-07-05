e0868d19e99abbcfd1e71e69611319203a4e2ec9
pedrodls <pedrojoaodev@gmail.com>
2022-07-05 11:24:52 +0100

    contact initial design

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,20 @@
+import  { useState, useEffect } from 'react';
+
+import Navbar from '../../components/navbar/Navbar';
+
+import Footer from '../../components/footer/Footer';
+
+export default function Contact() {  
+
+  
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+
+    return (
+        <>
+             <div>Sector dos contactos</div>
+
+        </>
+    )
+}
