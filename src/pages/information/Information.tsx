18103888b8fc105aff02c452ed8d52e954063938
pedrodls <pedrojoaodev@gmail.com>
2022-07-05 11:23:26 +0100

    information initial design

---
+++ src/pages/information/Information.tsx
@@ -0,0 +1,38 @@
+import styles from './Information.module.scss';
+
+import { Link, Outlet } from 'react-router-dom';
+import __VARIABLES__ from '../../envs/variables';
+import { FaArrowUp } from 'react-icons/fa';
+import { Button, ButtonGroup, Heading } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
+
+export default function Information() {  
+
+  
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+
+    return (
+        <>
+       
+            <div className={styles.background + " page-header header-filter"}>
+                <div className={styles.container + " container"}>
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <Heading as='h1' size='3xl'>
+                                <h1 className="title mb-5 mt-5">
+                                    Sector das Informações
+                                </h1>
+                            </Heading>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ backgroundColor: '#fff', height: '40vh' }}>
+                Informations
+            </div>
+        </>
+    )
+}
