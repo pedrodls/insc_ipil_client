dd5a0bf3ec3883f6093080e09130976d0c4e4c82
pedrodls <pedrojoaodev@gmail.com>
2022-07-05 11:25:24 +0100

    home initial design done

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,48 @@
+import styles from './Home.module.scss';
+
+import { Link, Outlet } from 'react-router-dom';
+import __VARIABLES__ from '../../envs/variables';
+import { FaArrowUp } from 'react-icons/fa';
+import { Button, ButtonGroup, Heading } from '@chakra-ui/react';
+
+export default function Home() {
+    return (
+        <>
+            <div className={`${styles.buttonUp}`}>
+                <FaArrowUp />
+            </div>
+            <div className={styles.background + " page-header header-filter"}>
+                <div className={styles.container + " container"}>
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <Heading as='h1' size='3xl'>
+                                <h1 className="title mb-5 mt-5">
+                                    Bem-vindo ao portal de <br /> inscrição online do
+                                    {' ' + __VARIABLES__._institute_short_name_}
+                                </h1>
+                            </Heading>
+
+                            <Link to={'/signup'} className="mt-5 me-3 mb-3">
+                                <Button colorScheme='red' className='text' size='lg'>
+                                    Inscrever-se agora!
+                                </Button>
+                            </Link>
+
+
+                            <Link to={'/signup'} className="mt-5 me-3 mb-3">
+                                <Button colorScheme={__VARIABLES__._orange_default_btn_} className='text' size='lg'>
+                                    Ver mais
+                                </Button>
+                            </Link>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ backgroundColor: '#fff', height: '40vh' }}>
+                home
+            </div>
+        </>
+    )
+}
