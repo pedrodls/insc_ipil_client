ad0d126334e59a74d3599e27787a2b0c7de20b72
pedrodls <pedrojoaodev@gmail.com>
2022-07-05 11:28:15 +0100

    all routes to initial page done

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,21 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Index, Login, SignUp } from './envs/elements';
+import { Index, Login, SignUp, Home, Information, AttachPayment, Contact } from './envs/elements';
+
 
 function AppRoutes() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Index />}>
+          <Route path="" element={<Home />} />
+          <Route path="/informations" element={<Information />} />
+          <Route path="/attach_payment" element={<AttachPayment />} />
+          <Route path="/contacts" element={<Contact />} />
+        </Route>
         <Route path="/login" element={<Login />} />
+
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
