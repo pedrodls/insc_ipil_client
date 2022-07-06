02e2393ae1b3bd74486913949a3b08ef35f3134c
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-07-06 22:20:05 +0100

    yup structure defining

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,28 +1,51 @@
 import { useState } from 'react';
-import { Navbar, Form, Input, Select, Footer } from './../../envs/elements';
+import { Navbar, Form, Input, Select, Footer, Button } from './../../envs/elements';
 import styles from './SignUp.module.scss';
-import { FaArrowLeft, FaArrowRight, FaBoxOpen, FaUpload } from 'react-icons/fa';
+import { FaArrowLeft, FaArrowRight, FaBoxOpen } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Carousel } from 'react-bootstrap';
+import { schemaStage1 } from './../../envs/schemas';
 
 //schema definition
-const schema = yup.object({
-    fatherName: yup.string().required(),
-}).required();
+const schemaState = [yup.object().shape({
+    fatherBI: yup.string().required(),
+    fatherFullName: yup.string().required(),
+    fatherCity: yup.string().required(),
+    fatherDistrict: yup.string().required(),
+    email: yup.string().email(),
+    telephone1: yup.string().min(9).max(9).required(),
+    telephone2: yup.string().min(9).max(9).required()
+})
+];
+
+//await schema.isValid(42);
+
+/*const person = {
+    age: -17,
+    name: 'Bruno Fortunato Domingos Mateus'
+}
+
+const schema1 = yup.object().shape({
+    age: yup.number().positive().integer().required(),
+    name: yup.string().required()
+})
+
+console.log(schema1);
+//schema1.validate(person).then(value => console.log(value)).catch(err => console.log(err));
+*/
 
 export default function SignUp() {
-    const [error, setError] = useState<object>({ show: false, message: '' });
+    const [error, setError] = useState<object>({ show: false, message: '', color: '' });
     const [spinner, setSpinner] = useState<boolean>(false);
     const [index, setIndex] = useState<number>(0);
 
     //------------------upload elements------------------
     const [errorUpload, setErrorUpload] = useState({ state: false, color: '', message: '' })
 
-
     //testando react-hook-form
-    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schemaStage1) });
 
     const onSubmit = (data: any) => console.log(data);
 
@@ -35,7 +58,6 @@ export default function SignUp() {
             //{ schedule: e.target.files[0] }
             setSpinner(false);
         } catch (error) { console.log(error) }
-
     }
 
     return (
@@ -45,20 +67,70 @@ export default function SignUp() {
                 <div className="row">
                     <div className={`${styles['content-form']} d-flex justify-content-center mb-4`}>
                         <Form _class="sign_up">
-                            <nav className={styles.pagination}>
-                                {index === 0 && <h1 className="text-center">Dados do Encarregado de Educação</h1>}
-                                {index === 1 && <h1 className="text-center">Dados do Aluno</h1>}
-                                {index === 2 && <h1 className="text-center">Dados Acadêmicos</h1>}
-                                {index === 3 && <h1 className="text-center">Anexo de Documentos</h1>}
-                                <ul className="pagination">
-                                    <li className={`page-item`}><button onClick={() => setIndex(0)} className={`page-link ${index === 0 ? styles.active : ''}`}>1</button></li>
-                                    <li className={`page-item`}><button onClick={() => setIndex(1)} className={`page-link ${index === 1 ? styles.active : ''}`}>2</button></li>
-                                    <li className={`page-item`}><button onClick={() => setIndex(2)} className={`page-link ${index === 2 ? styles.active : ''}`}>3</button></li>
-                                    <li className={`page-item`}><button onClick={() => setIndex(3)} className={`page-link ${index === 3 ? styles.active : ''}`}>4</button></li>
-                                </ul>
-                            </nav>
-                            <hr className="row-global" />
+                            {index !== 0 && <>
+                                <nav className={styles.pagination}>
+                                    {index === 1 && <h1 className="text-center">Dados do Encarregado de Educação</h1>}
+                                    {index === 2 && <h1 className="text-center">Dados do Aluno</h1>}
+                                    {index === 3 && <h1 className="text-center">Dados Acadêmicos</h1>}
+                                    {index === 4 && <h1 className="text-center">Anexo de Documentos</h1>}
+                                    <ul className="pagination">
+                                        <li className={`page-item`}><button onClick={() => setIndex(1)} className={`page-link ${index === 1 ? styles.active : ''}`}>1</button></li>
+                                        <li className={`page-item`}><button onClick={() => setIndex(2)} className={`page-link ${index === 2 ? styles.active : ''}`}>2</button></li>
+                                        <li className={`page-item`}><button onClick={() => setIndex(3)} className={`page-link ${index === 3 ? styles.active : ''}`}>3</button></li>
+                                        <li className={`page-item`}><button onClick={() => setIndex(4)} className={`page-link ${index === 4 ? styles.active : ''}`}>4</button></li>
+                                    </ul>
+                                </nav>
+                                <hr className="row-global" />
+                            </>}
                             <Carousel activeIndex={index} interval={300} indicators={false} slide={false} fade controls={false}>
+                                <Carousel.Item>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <h1>Áreas de Formação</h1>
+                                        <div className="d-flex justify-content-end" style={{ width: '40%' }}>
+                                            <button onClick={() => setIndex(1)} className="btn btn-danger">Começar</button>
+                                        </div>
+                                    </div>
+                                    <div className={`mt-4 mb-4`}>
+                                        <Select text="" id="cmbSexo" _options={[{ id: 'M', name: 'Informática' }, { id: 'F', name: 'Mecânica' }]} handle={handleSelect} _class="signup" />
+                                    </div>
+                                    <table className={`table table-borderless table-responsible ${styles.table}`} style={{ verticalAlign: 'middle', marginTop: '4em' }}>
+                                        <thead className="text-center">
+                                            <tr>
+                                                <th>-</th>
+                                                <th>Áreas</th>
+                                                <th>Estado das Guias</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="text-center">1</td>
+                                                <td>Construção Civil</td>
+                                                <td className="text-center text-danger">indisponível</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="text-center">2</td>
+                                                <td>Electricidade</td>
+                                                <td className="text-center text-success">disponível</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="text-center">3</td>
+                                                <td>Informática</td>
+                                                <td className="text-center text-success">disponível</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="text-center">4</td>
+                                                <td>Química</td>
+                                                <td className="text-center text-success">disponível</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="text-center">5</td>
+                                                <td>Mecânica</td>
+                                                <td className="text-center text-danger">indisponível</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </Carousel.Item>
                                 {/*------------------*Dados do Encarregado de Educação-----*/}
                                 <Carousel.Item>
                                     <div className="row">
@@ -209,7 +281,7 @@ export default function SignUp() {
                                 </Carousel.Item>
                             </Carousel>
 
-                            <nav className={`${styles.options} mt-4`}>
+                            {index !== 0 && <nav className={`${styles.options} mt-4`}>
                                 <ul className="pagination">
                                     <li className={`page-item`} onClick={() => { window.scrollTo(0, 0); setIndex(index - 1) }}>
                                         <button className="page-link text-center">
@@ -222,7 +294,7 @@ export default function SignUp() {
                                         </button>
                                     </li>
                                 </ul>
-                            </nav>
+                            </nav>}
                         </Form>
 
                         {/*<form onSubmit={handleSubmit(onSubmit)}>
