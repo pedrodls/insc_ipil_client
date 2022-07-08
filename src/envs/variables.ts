2961a9d235ae4b67dfbea2834b677ed20f90fc83
elisiomualumene <elisiomualumene@gmail.com>
2022-07-08 13:04:03 +0100

    :zap: new component

--- src/envs/variables.ts
+++ src/envs/variables.ts
@@ -4,7 +4,8 @@ const __VARIABLES__ = {
     _orange_default_btn_: "orange",
     _institute_phone_: '+244 934 054 182',
     _institute_email_: 'ipil.makarenko.co.ao',
-    _institute_address_: 'Largo Sweto'
+    _institute_address_: 'Largo Sweto',
+    _portal_name_: "INSCIPIL"
 }
 
 export default __VARIABLES__;
