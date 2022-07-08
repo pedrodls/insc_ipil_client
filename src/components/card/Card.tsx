c28f28561ce43747695259524d1657d4d3d94c64
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-07-08 11:29:47 +0100

    card component done

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,7 +1,17 @@
+import styles from './Card.module.scss';
+
 export default function Card(){
     return (
         <>
             Card
         </>
+    )
+}
+
+export function CardSignUp({_class="sign_up", children}: any){
+    return (
+        <div className={`${styles[_class]}`}>
+            {children}
+        </div>
     )
 }
