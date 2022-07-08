9d7ac607366eb19c7c060674aa2f44b0e8afccb9
elisiomualumene <elisiomualumene@gmail.com>
2022-07-08 03:18:08 +0100

    :sparkles: dashContent and dash-main

---
+++ src/pages/Admin/Dashboard/DashContent.tsx
@@ -0,0 +1,62 @@
+import {
+    Container,
+    Grid,
+    Paper,
+  } 
+  from '@mui/material';
+
+import Chart from '../../../components/Admin/Charts/Chart';
+import Deposits from '../../../components/Admin/Card/Deposits';
+import Orders from '../../../components/Admin/Table/Orders';
+import {Footer} from "../../../components/Admin/Footer"
+
+
+export const DashContent = () => {
+    return (
+        <>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3} >
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9} >
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <Chart />
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <Deposits />
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Orders />
+            </Paper>
+          </Grid>
+              {/* OUTRO :) */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Orders />
+            </Paper>
+          </Grid>
+        </Grid>
+        <Footer sx={{ pt: 4 }}/>
+      </Container>
+      </>
+    )
+}
