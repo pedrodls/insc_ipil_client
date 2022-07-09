0d3b46ce7e7124ed29135d23101811c264bdc9ce
pedrodls <pedrojoaodev@gmail.com>
2022-07-09 13:51:09 +0100

    App routes and routes updated

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Outlet, Route, Routes } from 'react-router-dom'
+import { Index, Login, SignUp, Home, Information, AttachPayment, Contact, Dashboard, UserAccount } from './environments/elements'
+
+function AppRoutes() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />}>
+          <Route path="" element={<Home />} />
+          <Route path="/informations" element={<Information />} />
+          <Route path="/attach_payment" element={<AttachPayment />} />
+          <Route path="/contacts" element={<Contact />} />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+
+        <Route path="/admin/new_account" element={<UserAccount />} />
+
+        <Route path="/dashboard" element={<Outlet />}>
+          <Route path="guides" element={<Home />} />
+          <Route path="validate_payment" element={<Information />} />
+          <Route path="attach_valid_payment" element={<AttachPayment />} />
+        </Route>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRoutes
