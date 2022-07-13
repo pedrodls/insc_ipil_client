7de58a409c4cda72ab068af3d3cedc8781a7e9a1
pedrodls <pedrojoaodev@gmail.com>
2022-07-13 01:48:54 +0100

    Guides dashboard fixed

--- src/pages/dashboards/guides/Guides.tsx
+++ src/pages/dashboards/guides/Guides.tsx
@@ -1,13 +1,54 @@
 
+import moment from 'moment'
 
+import { CardSignUp, CardGuides, Input, Select, InputDate } from '../../../environments/elements'
 
-export function Guides() {
+import styles from './Guides.module.scss'
+
+export function GuidesPage() {
 
 
     return <>
-    
-        Dashboard do camarada que vai enviar as Guias
-        
+        <div className={styles['container-fluid']}>
+
+            <div className="row">
+                <div className="col-md-12 d-flex justify-content-center">
+                    <CardGuides>
+
+                        <div className="row mb-4">
+                            <div className="col-md-6">
+
+                                <Input text="Nº de solicitação" type="text" name="codeSolicitation" placeholder="" _class="signup" />
+
+
+
+                            </div>
+                            <div className="col-md-6">
+
+                                <Input text="Rupe" type="text" name="educatorFullName" placeholder="" _class="signup" />
+
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-6">
+
+                                <Select text="Área de Formação" id="kinship" _options={[{ id: 'Mãe', name: 'Mãe' }, { id: 'Pai', name: 'Pai' }]} _class="signup" />
+
+
+                            </div>
+                            
+                        </div>
+
+
+                    </CardGuides>
+
+                </div>
+            </div>
+        </div>
+
+
+
     </>
-    
+
 }
