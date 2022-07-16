60aa591f6b544d04356605ae6dfe5e97c7c9c2cf
pedrodls <pedrojoaodev@gmail.com>
2022-07-16 22:03:02 +0100

    DAshboard updated

--- src/pages/dashboards/Dashboard.tsx
+++ src/pages/dashboards/Dashboard.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom";
-import { Footer, Navbar } from "../../environments/elements"; 1
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Footer, Navbar } from "../../environments/elements";
+import { UserDashboardRoutes } from "../../environments/routes";
+
 import styles from './Dashboard.module.scss'
 
 
 export function DashboardPage() {
 
 
-    return <>
+    const navigate = useNavigate()
 
+    useEffect(() => {
+
+        //Redirecionar na rota que tem acesso, ou seja, a que tem sessão!
+
+        if(window.location.pathname == '/dashboard')
+            navigate(UserDashboardRoutes[0])
+
+    }, [])
+
+    return <>
 
 
         <Navbar isSignUp={true} />
@@ -15,7 +28,6 @@ export function DashboardPage() {
         <Outlet />
 
         <Footer />
-
 
     </>
 
