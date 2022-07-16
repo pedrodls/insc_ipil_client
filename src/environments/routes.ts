79d6669891ec06c4880c7d867b7f6007dff63e05
pedrodls <pedrojoaodev@gmail.com>
2022-07-16 22:00:43 +0100

    routes fixed

---
+++ src/environments/routes.ts
@@ -0,0 +1,11 @@
+
+
+export const UserDashboardRoutes = [
+    'guides', 'validate_payment', 'attach_valid_payment'
+]
+
+export const SuperUserDashboardsRoutes = [
+    'guides', 'validate_payment', 'attach_valid_payment'
+]
+
+//O servidor vai enviar o tipo de rota de acesso e na rota própria será executado uma verificação no servidor para se conseguir lidar com apenas o acesso específico.
