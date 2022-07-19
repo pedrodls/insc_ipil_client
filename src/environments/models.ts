5c64ed37bf8d3fc9b6d40b07fcbfe7044f38a628
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-07-19 09:22:16 +0100

    add userModel, change town to TownId schem

--- src/environments/models.ts
+++ src/environments/models.ts
@@ -1,9 +1,11 @@
+import UserModel from '../models/user';
 import AreaModel from '../models/area';
 import TownModel from '../models/town';
 import CourseModel from '../models/course';
 
 
 export {
+    UserModel,
     AreaModel, 
     TownModel,
     CourseModel
