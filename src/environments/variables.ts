6abb4fb943f9ac5f82253af35e47b26b83f436d8
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-07-20 11:46:06 +0100

    added green color in variables

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -2,6 +2,7 @@ const __VARIABLES__ = {
     _institute_name_: "INSTITUTO POLITÉCNICO INDUSTRIAL DE LUANDA",
     _institute_short_name_: "IPIL",
     _orange_default_btn_: "orange",
+    _green_default_btn_: "#4CAF50",
     _institute_phone_: '+244 934 054 182',
     _institute_email_: 'ipil.makarenko.co.ao',
     _institute_address_: 'Largo Sweto',
