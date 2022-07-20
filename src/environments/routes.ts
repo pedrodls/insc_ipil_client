45189efdae508ab31d8645f3ba0697e455da179a
pedrodls <pedrojoaodev@gmail.com>
2022-07-20 12:33:22 +0100

    Routes env fixed

--- src/environments/routes.ts
+++ src/environments/routes.ts
@@ -1,11 +1,12 @@
 
 
-export const UserDashboardRoutes = [
-    'guides', 'validate_payment', 'attach_valid_payment'
-]
+export const UserDashboardRoutes: any = {
+    guide: "guides"
+}
 
 export const SuperUserDashboardsRoutes = [
     'guides', 'validate_payment', 'attach_valid_payment'
 ]
+
 
 //O servidor vai enviar o tipo de rota de acesso e na rota própria será executado uma verificação no servidor para se conseguir lidar com apenas o acesso específico.
