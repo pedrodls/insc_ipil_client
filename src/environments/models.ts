11314dfd06d0b3e48328084e86dc86ea38acda7b
Bruno Mateus <brunofortunatomateus@gmail.com>
2022-07-20 11:44:54 +0100

    subjectApplyModel implement

--- src/environments/models.ts
+++ src/environments/models.ts
@@ -2,11 +2,12 @@ import UserModel from '../models/user';
 import AreaModel from '../models/area';
 import TownModel from '../models/town';
 import CourseModel from '../models/course';
-
+import SubjectApplyModel from '../models/subject-apply';
 
 export {
     UserModel,
     AreaModel, 
     TownModel,
-    CourseModel
+    CourseModel,
+    SubjectApplyModel
 };
