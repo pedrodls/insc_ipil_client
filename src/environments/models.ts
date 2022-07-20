2b01364e331ccc2c3160e9a0bb6ddc240e73c7cd
pedrodls <pedrojoaodev@gmail.com>
2022-07-20 12:37:23 +0100

    Models update

--- src/environments/models.ts
+++ src/environments/models.ts
@@ -1,10 +1,14 @@
+import LoginModel  from './../models/login';
 import AreaModel from '../models/area';
 import TownModel from '../models/town';
 import CourseModel from '../models/course';
+import UserModel from '../models/user';
 
 
 export {
     AreaModel, 
     TownModel,
-    CourseModel
+    CourseModel,
+    UserModel,
+    LoginModel
 };
