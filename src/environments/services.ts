6114f4c53270eafb33b0a1e8dde9b4d23d1bb358
pedrodls <pedrojoaodev@gmail.com>
2022-07-20 12:36:13 +0100

    Services env fixed

--- src/environments/services.ts
+++ src/environments/services.ts
@@ -1,9 +1,12 @@
+import { AuthService } from './../services/main/AuthService';
 import { AppService } from "../services/AppService"; //main service
 
 //Services With Models
 import { AreaService} from "../services/main/AreaService";
 import { TownService } from "../services/main/TownService";
 import { CourseService } from '../services/main/CourseService';
+import { LocalStorageService } from '../services/main/LocalStorageService';
+import { TypeAccountService } from './../services/main/TypeAccountService';
 
 export {AppService};
-export {AreaService, TownService, CourseService};
+export {AreaService, TownService, CourseService, LocalStorageService, TypeAccountService, AuthService};
